def252cd6c876aeb318280f1e8b85596432a4948
Stephen Giovanni Saputra <stephen.gsaputra@gmail.com>
2022-04-09 21:15:35 +0700

    Add WorkSessionViewController

---
+++ Youra/sessionsGroup/WorkSessionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WorkSessionViewController.swift
+//  Youra
+//
+//  Created by Stephen Giovanni Saputra on 09/04/22.
+//
+
+import UIKit
+
+class WorkSessionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
