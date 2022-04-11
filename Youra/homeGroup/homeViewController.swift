39dd6ab2608a38169c973e5e99cf8ad5dd14504c
hnslmp <ancemo14@gmail.com>
2022-04-11 11:45:12 +0700

    fix/durationhomeview

--- Youra/homeGroup/homeViewController.swift
+++ Youra/homeGroup/homeViewController.swift
@@ -12,28 +12,25 @@ class homeViewController: UIViewController {
     @IBOutlet weak var workLabel: UILabel!
     @IBOutlet weak var restLabel: UILabel!
     
-	@IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
 
-	
-	var workDuration: TimeInterval? = 0
+    var workDuration: TimeInterval? = 0
     var restDuration: TimeInterval? = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Home Screen Loaded")
 
-		//Set Display Name
-		usernameLabel.text = UserDefaultManager.shared.defaults.value(forKey: "username") as? String
+        //Set Display Name
+        usernameLabel.text = UserDefaultManager.shared.defaults.value(forKey: "username") as? String
         
         NotificationCenter.default.addObserver(self, selector: #selector(self.funcName), name: NSNotification.Name(rawValue: "pickerClosed"), object: nil)
         
-        workLabel.text = stringFromTimeInterval(interval: workDuration!)
-        restLabel.text = stringFromTimeInterval(interval: workDuration!)
+        updateLabel()
     }
     
     @objc func funcName() {
-        workLabel.text = stringFromTimeInterval(interval: workDuration!)
-        restLabel.text = stringFromTimeInterval(interval: restDuration!)
+      updateLabel()
     }
     
 
@@ -58,5 +55,12 @@ class homeViewController: UIViewController {
         
         return String(timeStringed)
     }
-}
+    
+    func updateLabel(){
+        workDuration = UserDefaultManager.shared.defaults.value(forKey: "workSession") as? TimeInterval
+        restDuration = UserDefaultManager.shared.defaults.value(forKey: "restSession") as? TimeInterval
 
+        workLabel.text = stringFromTimeInterval(interval: workDuration!)
+        restLabel.text = stringFromTimeInterval(interval: restDuration!)
+    }
+}
