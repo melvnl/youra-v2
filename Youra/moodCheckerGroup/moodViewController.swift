410913a2b1efe8e46bc4553dfa5447e8d54db6c8
himelv <63998835+melvnl@users.noreply.github.com>
2022-04-11 13:00:26 +0700

    feat/mood-checker (#7)
    * initialize add mood image, and render emoji based on slider value
    
    * chores: change mood label text
    
    * revert default initial view controller

---
+++ Youra/moodCheckerGroup/moodViewController.swift
@@ -0,0 +1,59 @@
+//
+//  moodViewController.swift
+//  Youra
+//
+//  Created by melvin on 11/04/22.
+//
+
+import UIKit
+
+class moodViewController: UIViewController {
+
+    @IBOutlet weak var emoji: UIImageView!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var moodLabel: UILabel!
+    
+    @IBAction func getSliderValue(_ sender: UISlider) {
+        let currentValue = Float(sender.value)
+        
+        if(currentValue >= 0.75){
+            let image = UIImage(named:"emoji 4.png")
+            emoji.image = image
+            moodLabel.text = "Refreshed and happy"
+        }
+        else if(currentValue >= 0.5){
+            let image = UIImage(named:"emoji 3.png")
+            emoji.image = image
+            moodLabel.text = "I’m fine"
+        }
+        else if(currentValue >= 0.25){
+            let image = UIImage(named:"emoji 2.png")
+            emoji.image = image
+            moodLabel.text = "Not okay"
+        }
+        else{
+            let image = UIImage(named:"emoji 1.png")
+            emoji.image = image
+            moodLabel.text = "Very Bad"
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
