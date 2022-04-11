c93509b6a647b38a3c71e3a6c92adb900e19a78b
stephengsaputra <76246626+stephengsaputra@users.noreply.github.com>
2022-04-11 09:37:47 +0700

    Work and Rest Session ViewController and Storyboard (#4)
    * Add WorkSessionViewController
    
    * add rest session view, background sets, quotes sets
    
    Co-authored-by: himelv <63998835+melvnl@users.noreply.github.com>

---
+++ Youra/sessionsGroup/BackgroundModel.swift
@@ -0,0 +1,25 @@
+//
+//  BackgroundModel.swift
+//  Youra
+//
+//  Created by Stephen Giovanni Saputra on 10/04/22.
+//
+
+import Foundation
+import UIKit
+
+struct Background {
+    
+    var bgTitle: UIImage?
+    var backgroundLayerColor: UIColor
+    var foregroundLayerColor: UIColor
+    var timerLabelColor: UIColor
+    var circularViewColor: UIColor
+    var noteButtonBGColor: UIColor
+    var noteIconColor: UIColor
+}
+
+struct Quote {
+    
+    var quote: String
+}
