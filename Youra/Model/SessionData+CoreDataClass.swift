b53416a1a4eb3dd83bc6188526e44618f3b1f646
Rahmat Afriyanton <54926315+rahmatafriyanton@users.noreply.github.com>
2022-04-12 22:12:32 +0700

    Core data (#15)
    * Core data
    
    * Core data

---
+++ Youra/Model/SessionData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  SessionData+CoreDataClass.swift
+//  Youra
+//
+//  Created by Rahmat Afriyanton on 12/04/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(SessionData)
+public class SessionData: NSManagedObject {
+
+}
