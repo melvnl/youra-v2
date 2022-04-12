7e7997bfbbda168575c03f4e55b5e4d32b9581d3
stephengsaputra <76246626+stephengsaputra@users.noreply.github.com>
2022-04-12 17:20:37 +0700

    Fix Navigation Controller Bug (#13)
    * Add notes screen
    
    * fix app flow
    
    * Connect notes to RestSessionView
    
    * Fix Navigation Controller
    
    Co-authored-by: Rahmat Afriyanton <rahmatafriyanton@gmail.com>

--- Youra/moodCheckerGroup/moodViewController.swift
+++ Youra/moodCheckerGroup/moodViewController.swift
@@ -58,15 +58,14 @@ class moodViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
     }
-    */
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
 
 }
