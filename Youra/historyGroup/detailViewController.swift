9e75cfe5715824f24fb24abac33551f611d76f7a
Stephen Giovanni Saputra <stephen.gsaputra@gmail.com>
2022-07-02 19:36:17 +0700

    Fix detailVC bug

--- Youra/historyGroup/detailViewController.swift
+++ Youra/historyGroup/detailViewController.swift
@@ -25,14 +25,10 @@ class detailViewController: UIViewController {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "HH:mm"
         
-        let start = dateFormatter.date(from: AppHelper.getStringTime(date: (detail?.createDate)!))
-        let end = dateFormatter.date(from: AppHelper.getStringTime(date: (detail?.createDate)!))
-        dateFormatter.dateFormat = "h:mm a"
-      
 		noteTitle.text = detail?.noteTitle
 		noteBody.text  = detail?.noteBody
-		startTime.text = dateFormatter.string(from: start!)
-		endTime.text = dateFormatter.string(from: end!)
+        startTime.text = dateFormatter.string(from: (detail?.createDate)!)
+        endTime.text = dateFormatter.string(from: (detail?.endDate)!)
 		moodBefore.image = AppHelper.setMoodImage(mood: detail!.moodBefore)
 		moodAfter.image = AppHelper.setMoodImage(mood: detail!.moodAfter)
 		
